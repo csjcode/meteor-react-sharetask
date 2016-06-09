c4607d3bff4d8fe6cac6042b09cda1dfb49297d7
csjcode <christophersstjohn@gmail.com>
2016-06-09 23:40:06 +0700

    Added Meteor Method for inserting data (5.82)

--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -1,4 +1,15 @@
 // Declare our collection
 import { Mongo } from 'meteor/mongo';
 
+Meteor.methods ({
+   'bins.insert':function () {
+      return Bins.insert({
+         createdAt: new Date(),
+         content: '',
+         sharedWith: [],
+         ownerId: this.userId
+      });
+   }
+})
+
 export const Bins = new Mongo.Collection('bins');
