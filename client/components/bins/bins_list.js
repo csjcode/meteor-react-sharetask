effd5c8027fe9c12b4da94c514f63975e630bbb5
csjcode <christophersstjohn@gmail.com>
2016-06-10 17:30:17 +0700

    Render BinsList component

--- client/components/bins/bins_list.js
+++ client/components/bins/bins_list.js
@@ -6,10 +6,22 @@ class BinsList extends Component {
    constructor() {
       super();
    }
+   renderList(){
+      return this.props.bins.map(bin => {
+         return (
+            <li className="list-group-item" key={bin._id}>
+               Bin {bin._id}
+            </li>
+         );
+      });
+
+   }
    render() {
-      console.log(this.props.bins)
+      // console.log(this.props.bins)
       return (
-         <div>BinsList</div>
+         <ul className="list-group">
+            {this.renderList()}
+         </ul>
       );
    }
 
