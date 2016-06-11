443a04c1f4c983a50636ac7a1103864aadcca456
csjcode <christophersstjohn@gmail.com>
2016-06-11 13:19:04 +0700

    Added /bins/ Route (5.89)

---
+++ client/components/bins/bins_main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BinsMain extends Component {
+   constructor() {
+      super();
+   }
+   render(){
+      return (
+         <div>BinsMain</div>
+      );
+   }
+
+}
+
+export default BinsMain;
