34269961ebca17dc0369fa844b9eaf74d090cfe1
csjcode <christophersstjohn@gmail.com>
2016-06-11 13:51:59 +0700

    Added binId  token to Routes

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -4,7 +4,9 @@ class BinsMain extends Component {
    constructor() {
       super();
    }
+
    render(){
+      // console.log(this.props.params.binId);
       return (
          <div>BinsMain</div>
       );
