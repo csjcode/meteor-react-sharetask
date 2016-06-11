d498d13f364774dba55f315552bfc7042a3c0202
csjcode <christophersstjohn@gmail.com>
2016-06-11 16:37:30 +0700

    Added BinsViewer component

---
+++ client/components/bins/bins_viewer.js
@@ -0,0 +1,16 @@
+import React,{ Component } from 'react';
+
+class BinsViewer extends Component {
+   constructor() {
+      super();
+   }
+   render(){
+      return (
+         <div className="col-xs-4">
+            <h5>Viewer</h5>
+         </div>
+      );
+   }
+}
+
+export default BinsViewer;
