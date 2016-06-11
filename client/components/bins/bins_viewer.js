9dfa5517ed1fa018057eb99d041db96f77e2a4d7
csjcode <christophersstjohn@gmail.com>
2016-06-11 17:32:09 +0700

    Updated BinsViewer to display markdown properly

--- client/components/bins/bins_viewer.js
+++ client/components/bins/bins_viewer.js
@@ -1,13 +1,16 @@
 import React,{ Component } from 'react';
+import { markdown } from 'markdown';
 
 class BinsViewer extends Component {
    constructor() {
       super();
    }
    render(){
+      const rawHTML = markdown.toHTML(this.props.bin.content);
       return (
          <div className="col-xs-4">
             <h5>Viewer</h5>
+            <div dangerouslySetInnerHTML={{__html:rawHTML}}></div>
          </div>
       );
    }
