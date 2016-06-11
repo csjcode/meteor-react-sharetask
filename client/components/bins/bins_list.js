fabb4c59816e28e4e0499ddefaa068ad2b9e08ab
csjcode <christophersstjohn@gmail.com>
2016-06-11 12:13:15 +0700

    Add capability to Remove bins (5.87)

--- client/components/bins/bins_list.js
+++ client/components/bins/bins_list.js
@@ -6,11 +6,21 @@ class BinsList extends Component {
    constructor() {
       super();
    }
+   onBinRemove(bin){
+            Meteor.call('bins.remove', bin)
+   }
    renderList(){
       return this.props.bins.map(bin => {
          return (
             <li className="list-group-item" key={bin._id}>
                Bin {bin._id}
+               <span className="pull-right">
+                  <button
+                     className="btn btn-danger"
+                     onClick={() => this.onBinRemove(bin)}>
+                     Remove
+                  </button>
+               </span>
             </li>
          );
       });
--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -9,7 +9,11 @@ Meteor.methods ({
          sharedWith: [],
          ownerId: this.userId
       });
+   },
+   'bins.remove':function (bin) {
+      return Bins.remove(bin);
    }
-})
+
+});
 
 export const Bins = new Mongo.Collection('bins');
